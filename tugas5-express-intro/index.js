7f6b94fb5de504631e980925181fe0cabc312b02
krisnabimantoro <krisnabmntr@gmail.com>
2024-07-20 01:34:06 +0700

    first commit

---
+++ tugas5-express-intro/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+app.get("/hello", (req, res) => {
+  res.json({
+    message: "Success fetch message",
+    data: "Hello World!",
+  });
+});
+
+app.get("/user", (req, res) => {
+  res.json({
+    message: "Succes fetch message",
+    data: {
+      id: 1,
+      name: "Budi",
+      username: "budidu",
+      email: "budidu@mail.com",
+    },
+  });
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`Server is running at <http://localhost>:${port}`);
+});
