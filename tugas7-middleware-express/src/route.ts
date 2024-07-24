1044c4e25de845363582d2ceae1c07ab80728afa
krisnabimantoro <krisnabmntr@gmail.com>
2024-07-24 18:57:13 +0700

    updated

---
+++ tugas7-middleware-express/src/route.ts
@@ -0,0 +1,46 @@
+import express from "express";
+
+import { single, multiple } from "./middlewares/upload.middleware";
+import { handleUpload } from "./utils/cloudinary";
+
+
+const router = express.Router();
+
+
+router.post("/upload/single", single, async(req, res) => {
+  const file = req.file
+
+  if (file) {
+    try {
+      const result = await handleUpload(file.path)
+      res.status(200).send(result)
+      
+    } catch (error) {
+      res.status(500).send({error:'File not uploaded'})
+    }
+   
+  }else{
+    res.status(400).send({ error: 'No file uploaded' })
+  }
+
+  
+});
+router.post("/upload/multiple", multiple, async(req, res) => {
+  const files = req.files as Express.Multer.File[];
+
+  if (files && files.length > 0) {
+    try {
+      const uploadPromises = files.map(file => handleUpload(file.path));  
+      const results = await Promise.all(uploadPromises);
+      res.status(200).send(results);
+    } catch (err) {
+      res.status(500).send({ error: 'Upload failed' });
+    }
+  } else {
+    res.status(400).send({ error: 'No files uploaded' });
+  }
+});
+
+
+
+export default router;
---
+++ tugas7-middleware-express/src/utils/env.ts
@@ -0,0 +1,12 @@
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "897539643389373";
+export const CLOUDINARY_API_SECRET: string =
+  process.env.CLOUDINARY_API_SECRET || "3B_09teXBspD49OxcI-gNCfqAS8";
+export const CLOUDINARY_CLOUD_NAME: string =
+  process.env.CLOUDINARY_CLOUD_NAME || "dbot4jz71";
+
+
+// export const env = {
+//   CLOUDINARY_CLOUD_NAME: 'dbot4jz71',
+//   CLOUDINARY_API_KEY: '897539643389373',
+//   CLOUDINARY_API_SECRET: '3B_09teXBspD49OxcI-gNCfqAS8',
+// };
