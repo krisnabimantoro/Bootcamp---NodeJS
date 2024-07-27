c1492b6ce0d5c1e8dd40a0280997a78601fe4a18
Krisna Bimantoro <70993224+krisnabimantoro@users.noreply.github.com>
2024-07-28 00:20:13 +0700

    Delete tugas8-db-nosql-mongodb/src/utils/env.ts

--- tugas8-db-nosql-mongodb/src/utils/env.ts
+++
@@ -1,2 +0,0 @@
-export const DATABASE_URL: string =
-  "mongodb+srv://krisnabmntr:PmqcA6ci98MJmsT3@belajar-backend-nodejs.vzey3tq.mongodb.net/?retryWrites=true&w=majority&appName=belajar-backend-nodejs";
