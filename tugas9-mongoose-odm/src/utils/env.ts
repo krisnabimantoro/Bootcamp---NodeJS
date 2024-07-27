b821e97cc6c8ae0fd34216d1007bdee38d2bfe1b
Krisna Bimantoro <70993224+krisnabimantoro@users.noreply.github.com>
2024-07-28 00:19:53 +0700

    Delete tugas9-mongoose-odm/src/utils/env.ts

--- tugas9-mongoose-odm/src/utils/env.ts
+++
@@ -1,9 +0,0 @@
-export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "897539643389373";
-export const CLOUDINARY_API_SECRET: string =
-  process.env.CLOUDINARY_API_SECRET || "3B_09teXBspD49OxcI-gNCfqAS8";
-export const CLOUDINARY_CLOUD_NAME: string =
-  process.env.CLOUDINARY_CLOUD_NAME || "dbot4jz71";
-
-
-export const DATABASE_URL: string =
-  "mongodb+srv://krisnabmntr:PmqcA6ci98MJmsT3@belajar-backend-nodejs.vzey3tq.mongodb.net/?retryWrites=true&w=majority&appName=belajar-backend-nodejs";
