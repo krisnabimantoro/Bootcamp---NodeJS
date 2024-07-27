01c20424a919cc86f12bf29bcdefad07a6050ecc
krisnabimantoro <krisnabmntr@gmail.com>
2024-07-27 20:39:57 +0700

    updated

---
+++ tugas10-validation/src/route.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+import categoryController from "./controllers/categories.controller";
+import productsController from "./controllers/products.controller";
+
+const router = express.Router();
+
+router.get("/products", productsController.findAll);
+router.post("/products", productsController.create);
+router.get("/products/:id", productsController.findOne);
+router.put("/products/:id", productsController.update);
+router.delete("/products/:id", productsController.delete);
+
+router.get("/categories", categoryController.findAll);
+router.post("/categories", categoryController.create);
+router.get("/categories/:id", categoryController.findOne);
+router.put("/categories/:id", categoryController.update);
+router.delete("/categories/:id", categoryController.delete);
+
+export default router;
